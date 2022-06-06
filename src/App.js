9b62bd7141f1a42eedc2fddeb3b9ec55baeb7fc8
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-07 01:48:48 +0430

    table rendered

--- src/App.js
+++ src/App.js
@@ -1,61 +1,68 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import "./style.css";
-import StyledButton, {
-  FancyButton,
-  SubmitButton,
-  AnimatedLogo,
-  DarkButton,
-} from "./components/Button/Button";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+// import "./style.css";
+// import StyledButton, {
+//   FancyButton,
+//   SubmitButton,
+//   AnimatedLogo,
+//   DarkButton,
+// } from "./components/Button/Button";
+// import { ThemeProvider, createGlobalStyle } from "styled-components";
+import BasicTable from "./components/BasicTable";
 
-const theme = {
-  dark: {
-    primary: "#000",
-    text: "#fff",
-  },
-  light: {
-    primary: "#fff",
-    text: "#000",
-  },
-  fontFamily: "Segoe UI",
-};
+// const theme = {
+//   dark: {
+//     primary: "#000",
+//     text: "#fff",
+//   },
+//   light: {
+//     primary: "#fff",
+//     text: "#000",
+//   },
+//   fontFamily: "Segoe UI",
+// };
 
-const GlobalStyle = createGlobalStyle`
-button {
-  font-family: 'Roboto';
-  // we can also give the style from theme provider as props
-  font-family: ${(props) => props.theme.fontFamily};
-}
-`;
+// const GlobalStyle = createGlobalStyle`
+// button {
+//   font-family: 'Roboto';
+//   // we can also give the style from theme provider as props
+//   font-family: ${(props) => props.theme.fontFamily};
+// }
+// `;
+
+// const stylingTest = (
+//   <ThemeProvider theme={theme}>
+//     <GlobalStyle />
+//     <div className="App">
+//       {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//       {/* <button>Button</button> */}
+//       <AnimatedLogo src={logo} alt="logo" />
+//       <StyledButton>Styled Button</StyledButton>
+//       <div>
+//         <br />
+//       </div>
+//       <StyledButton variant="outline">Styled Button</StyledButton>
+//       <div>
+//         <br />
+//       </div>
+//       <FancyButton as="a">Fancy Button</FancyButton>
+//       <div>
+//         <br />
+//       </div>
+//       <SubmitButton>Submit Button</SubmitButton>
+//       <div>
+//         <br />
+//       </div>
+//       <DarkButton>Dark Theme</DarkButton>
+//     </div>
+//   </ThemeProvider>
+// );
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <div className="App">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <button>Button</button> */}
-        <AnimatedLogo src={logo} alt="logo" />
-        <StyledButton>Styled Button</StyledButton>
-        <div>
-          <br />
-        </div>
-        <StyledButton variant="outline">Styled Button</StyledButton>
-        <div>
-          <br />
-        </div>
-        <FancyButton as="a">Fancy Button</FancyButton>
-        <div>
-          <br />
-        </div>
-        <SubmitButton>Submit Button</SubmitButton>
-        <div>
-          <br />
-        </div>
-        <DarkButton>Dark Theme</DarkButton>
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <BasicTable />
+    </div>
   );
 }
 
--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,16 +1,48 @@
 import { useTable } from "react-table";
-import MOCK_DATA from "./MOCK_DATA.json"
-import COLUMNS from "./columns"
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
 import { useMemo } from "react";
-
+import "./table.css";
 const BasicTable = () => {
-    const columns = useMemo(() => columns, [])
-    const data = useMemo(() => data, [])
-    useTable({
-        columns: COLUMNS,
-        data: MOCK_DATA, 
-    })
-    return (  );
-}
- 
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  // step3: creating a table instance
+  const tableInstance = useTable({
+    // using es6 shorthand syntax
+    columns,
+    data,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  // step4: define a basic table structure
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
 export default BasicTable;
